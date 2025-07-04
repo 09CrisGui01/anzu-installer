998117bdeee07f594b204c745367b9f54e72809d
Cristian Guilarte <cristian.guilarte@proton.me>
2025-07-04 12:38:55 +0100

    docs: Add copyright claim

--- installer.sh
+++ installer.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+
+# anzu-installer: Script to customise a fresh Arch Linux installation.
+# Copyright (C) 2025  Cristian Guilarte
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
